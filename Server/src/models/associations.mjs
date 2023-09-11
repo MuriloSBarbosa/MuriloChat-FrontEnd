dee4e5616e96ee912fb091314f015ec742f54640
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-09-11 18:27:50 -0300

    Change server to another repository

--- Server/src/models/associations.mjs
+++
@@ -1,6 +0,0 @@
-import Sala from "./Sala.mjs";
-import Chat from "./Chat.mjs";
-import Usuario from "./Usuario.mjs";
-
-Usuario.hasMany(Chat, { foreignKey: 'fkUsuario' });
-Sala.hasMany(Chat, { foreignKey: 'fkSala' });
